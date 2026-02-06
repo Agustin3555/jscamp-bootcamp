fed46d23c831bb0847cff8421a0ce47217bd2fdb
Agustin3555 <Agustin3555@hotmail.com>
2026-02-06 11:35:58 -0300

    feat: Controla los paths válidos para mostrar el componente NotFount

--- 03-ejercicio-react/src/App.tsx
+++ 03-ejercicio-react/src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import { useRouter } from './hooks/useRouter.ts'
 import { Header } from './components/Header/Header.tsx'
 import { Footer } from './components/Footer/Footer.tsx'
 import { Route } from './components/Route/Route.tsx'
@@ -6,12 +7,25 @@ import Home from './pages/Home/Home.tsx'
 import Search from './pages/Search/Search.tsx'
 import NotFound from './pages/NotFound/NotFound.tsx'
 
-export const App = () => (
-  <div className="cmp-app">
-    <Header />
-    <Route path="/" component={<Home />} />
-    <Route path="/search" component={<Search />} />
-    <Route path="/*" component={<NotFound />} />
-    <Footer />
-  </div>
-)
+const VALID_PATHS = ['/', '/search']
+
+export const App = () => {
+  const { currentPath } = useRouter()
+
+  const isValidPath = VALID_PATHS.includes(currentPath)
+
+  return (
+    <div className="cmp-app">
+      <Header />
+      {isValidPath ? (
+        <>
+          <Route path="/" component={<Home />} />
+          <Route path="/search" component={<Search />} />
+        </>
+      ) : (
+        <NotFound />
+      )}
+      <Footer />
+    </div>
+  )
+}
